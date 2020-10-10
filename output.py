afd6351dfb873efdb98b0543a3679e4287ffa2b9
Gerasimos <44060598@users.noreply.github.com>
2020-10-10 16:58:35 +0300

    Finished timestamp graph generator

--- output.py
+++ output.py
@@ -2,6 +2,7 @@ import collections
 from datetime import datetime
 from time import time
 from matplotlib import pyplot
+from matplotlib import rcParams
 from typing import List, Callable, Tuple, AnyStr
 #TODO pdf
 
@@ -19,13 +20,17 @@ from typing import List, Callable, Tuple, AnyStr
 
 class DataPlotPoint:
 
-    def __init__(self, title : AnyStr, x, y, z = None, receiver = None):
+    def __init__(self, title : AnyStr, x, y, z = None, receiver = None, xlim = None, ylim = None, zlim = None):
         self.title : str = title
 
         self.x : List = x
         self.y : List = y
         self.z : List = z
         self.r = receiver
+        self.xlim : int = xlim
+        self.ylim : int = ylim
+        self.zlim : int = zlim
+
 
     def __str__(self):
         return f"Title: {self.title}\nReceiver: {self.r}\n\tX axis: {self.x}\n\tY axis: {self.y}\n\tZ axis: {self.z}"
@@ -41,7 +46,8 @@ class Generator:
 
         self.packet_plot_flag = False
         self.timestamp_plot_flag = False
-        self.graphs = []
+        self.timestamp_graphs = []
+        self.packet_graphs = []
 
     def start_new_pass(self, output_path):
         self.postfix = int(time())
@@ -50,7 +56,8 @@ class Generator:
         self.plot_count = 0
         self.packet_plot_flag = False
         self.timestamp_plot_flag = False
-        self.graphs = []
+        self.timestamp_graphs = []
+        self.packet_graphs = []
 
     def close(self):
         #clean up any files / reset all
@@ -81,18 +88,30 @@ class Generator:
 
         return servers_return
 
-    def get_all_data_points_for_ip_server_combo_from_timestamp(self, chunk, ip, server) -> Tuple[list, list]:
+    def get_all_data_points_for_ip_server_combo_from_timestamp(self, chunk, ip, server) -> Tuple[list, list, int]:
         from db.tables import Timeframe
         data = []
         dates = []
+        date_lim = 0
+        max_data_val = 0
 
         timestamp : Timeframe
         for timestamp in chunk:
             if timestamp.interface == ip and timestamp.receiver == server:
                 data.append(timestamp.ms)
                 dates.append(timestamp.datetime)
+                if timestamp.ms > max_data_val:
+                    max_data_val = timestamp.ms
 
-        return data, dates
+                if timestamp.limit > date_lim:
+                    date_lim = timestamp.limit
+
+        if date_lim < max_data_val:
+            lim = date_lim
+        else:
+            lim = max_data_val
+
+        return data, dates, lim
 
     def generate_timestamp_data_plot_obj_from(self, chunk,
                                               x_format:Callable[[List], List] = None,
@@ -113,14 +132,14 @@ class Generator:
                 server_readable = server_tuple[1]
 
                 name = f"Interface {ip if ip is not None else 'unknown'} server {server_readable}"
-                x, y = self.get_all_data_points_for_ip_server_combo_from_timestamp(chunk, ip, server_ip)
+                y, x, ylim = self.get_all_data_points_for_ip_server_combo_from_timestamp(chunk, ip, server_ip)
 
                 if x_format is not None:
                     x = x_format(x)
                 if y_format is not None:
                     y = y_format(y)
 
-                plotpoints.append(DataPlotPoint(name, x, y, receiver = server_readable))
+                plotpoints.append(DataPlotPoint(name, x, y, receiver = server_readable, ylim = ylim))
 
         return plotpoints
 
@@ -143,41 +162,41 @@ class Generator:
                     print(f"Writing:\n\t{line}")
 
     def generate_timestamp_graph(self, generator : collections.Generator):
-        #do stuff
-        index = 0
-        for chunk in generator:
-            graph_data = self.generate_timestamp_data_plot_obj_from(chunk)
+        #https://stackoverflow.com/questions/8270981/in-a-matplotlib-plot-can-i-highlight-specific-x-value-ranges
+        from db.tables import Timeframe
 
-            #fig, axes = pyplot.subplots(len(ips), 1, constrained_layout = True, figsize = (11.7,8.3))
-            #second pass, for each plot plot ip for each server
-            #for i in range(len(ips)):
-            #    for j in range(len(ips_data[i])):
-            #        #plot data, add to legend
-            #        print("#########################################")
-            #        print(f"Plotting {ips[i]}")
-            #        if len(ips) == 1:
-            #            axes.plot(ips_dates[i][j], ips_data[i][j], linewidth = "0.6")
-            #        else:
-            #            axes[i].plot(ips_dates[i][j], ips_data[i][j], linewidth = "0.6")
-            #    print("Setting misc...")
-            #    if len(ips) == 1:
-            #        axes.set_title(f"IP {ips[i] if ips[i] is not None else 'Dynamic'}")
-            #        axes.set_xlabel("Dates (YYYY-MM-DD HH-MM-SS)")
-            #        axes.set_ylabel("Ping (ms)")
-            #        axes.set_ylim((0, maxy))
-            #        axes.legend(receivers[i])
-            #    else:
-            #        axes[i].set_title(f"IP {ips[i] if ips[i] is not None else 'Dynamic'}")
-            #        axes[i].set_xlabel("Dates (YYYY-MM-DD HH-MM-SS)")
-            #        axes[i].set_ylabel("Ping (ms)")
-            #        axes[i].ylim((0, maxy))
-            #        axes[i].legend(receivers[i])
-            #fig.suptitle("Ping Server(s) Results")
-            #print(f"Outputting...{Generator.TIMESTAMP_INFIX}{index}{self.postfix}.jpg")
-            #fig.savefig(f"{self.output_path}{Generator.TIMESTAMP_INFIX}{index}{self.postfix}.jpg",
-            #            bbox_inches="tight", dpi = 150)
-            #pyplot.close(fig)
-            #self.graphs.append(f"{self.output_path}{Generator.TIMESTAMP_INFIX}{index}{self.postfix}.jpg")
+        index = 0
+        chunk : List[Timeframe]
+        for chunk in generator:
+            rcParams['figure.figsize'] = (11.7, 8.3)
+            pyplot.subplots_adjust(hspace=1.5)
+
+            graph_data : List[DataPlotPoint] = self.generate_timestamp_data_plot_obj_from(chunk)
+
+            pltnum = 1
+            suptitle = f"{chunk[0].datetime.strftime('%y-%b-%d : %H %M')} - " \
+                       f"{chunk[-1].datetime.strftime('%y-%b-%d : %H %M')}"
+            for i in range(len(graph_data)):
+                data = graph_data[i]
+                ax = pyplot.subplot(len(graph_data), 1, pltnum)
+
+                ax.plot(data.x, data.y)
+                ax.set_title(data.title)
+                ax.set_xlabel("Dates")
+                ax.set_ylabel("ms")
+
+                ax.set_ylim((0, data.ylim))
+                pyplot.suptitle(suptitle)
+
+                pltnum += 1
+            print(f"Outputting...{Generator.TIMESTAMP_INFIX}{index}{self.postfix}.jpg")
+            pyplot.savefig(f"{self.output_path}{Generator.TIMESTAMP_INFIX}{index}{self.postfix}.jpg",
+                           bbox_inches="tight", dpi = 300)
+            pyplot.close()
+
+            self.timestamp_graphs.append(f"{self.output_path}{Generator.TIMESTAMP_INFIX}{index}{self.postfix}.jpg")
+
+            index += 1
 
         self.timestamp_plot_flag = True
 
